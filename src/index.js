dd77daa6d67e2ffebbc1387bd14b4f225bc5e836
Andrii Berezhko <Berezhko7777@ukr.net>
2022-05-26 01:29:07 +0300

    done
    done

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import makeSearch from './search_api';
 import marckupTpl from './cardTpl';
 import { config } from './config_api';
 
+
 // Додатковий імпорт стилів
 import "notiflix/dist/notiflix-3.2.5.min.css";
 
@@ -24,36 +25,42 @@ refs.loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
 
 function onSearchFormSubmit (event) {
     event.preventDefault();
-    if (config.search !== event.currentTarget.elements.searchQuery.value ) {
-        config.search = event.currentTarget.elements.searchQuery.value;
-        config.page = 1;
-        refs.loadMoreBtn.classList.add('disabled');
-        clearMarckUp();
-    }
-   
-   
-    
-    return makeSearch(config).then(data => {
-        config.totalHits = data.totalHits - config.perPage;
-        console.log(config.totalHits);
-            if (data.totalHits === 0) {
-            return Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+    if (!inputFieldValidation(event)) {
+        return;
+    } ;
+      
+    makeSearch( config).then(data => {
+        
+        if (data.totalHits === 0) {
+            config.search = '';
+            return Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
         };
+       
         config.page += 1;
         refs.loadMoreBtn.classList.remove('disabled');
+        config.totalHits += config.perPage;
         makeMarckUp(data);         
            
-    });
+    })
+        .catch(error => errorHandling());
     
 };
 
 function onLoadMoreBtnClick() {
-    // console.log(config);
-    makeSearch(config).then(data => {
-        config.totalHits -= config.perPage;
+    
+    makeSearch( config).then(data => {
+        config.totalHits += config.perPage;
+        console.log(config.totalHits);
         makeMarckUp(data);
-    });
-    console.log(config.totalHits);
+        if (config.totalHits >= data.totalHits) {
+            refs.loadMoreBtn.classList.add('disabled');
+            config.search = '';
+            return Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+        };
+        
+    })
+        .catch(error => errorHandling());
+    
     config.page += 1;
 
 
@@ -66,5 +73,30 @@ function makeMarckUp(data) {
 
 function clearMarckUp() {
     refs.gallery.innerHTML = ' ';
-}
+};
+
+function inputFieldValidation(event) {
+    if (config.search !== event.currentTarget.elements.searchQuery.value) {
+        
+        config.search = event.currentTarget.elements.searchQuery.value;
+        config.page = 1;
+        refs.loadMoreBtn.classList.add('disabled');
+        config.totalHits = 0;
+        clearMarckUp();
+        return true;
+    };
+    if (config.search === '') {
+        clearMarckUp();
+        Notiflix.Notify.failure("Введи что нибудь!!!");
+        return false;
+    };
+};
+
+function errorHandling() {
+    clearMarckUp();
+            config.search = '';
+            refs.loadMoreBtn.classList.add('disabled');
+        return Notiflix.Notify.failure("Что то пошло не так((("); 
+};
+
 
--- src/search_api.js
+++ src/search_api.js
@@ -1,10 +1,17 @@
 
 const axios = require('axios').default;
 
-export default function makeSearch( {search,apiKey,page,perPage} ) {
-    const url = `https://pixabay.com/api/?key=${apiKey}&q=${search}&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
-    return axios.get(url)
-        .then(response => {
-         return response.data;
-        }); 
+export default async function  makeSearch({baseUrl,search,apiKey,page,perPage} ) {
+    const url = `${baseUrl}&key=${apiKey}&q=${search}&page=${page}&per_page=${perPage}`;
+    const response = await axios.get(url);
+    const data = await response.data;
+    return data;
+    // return axios.get(url)
+    //     .then(response => {
+    //         if (response.status !== 200) {
+                
+    //             throw new Error(response.statusText);
+    //         };
+    //      return response.data;
+    //     }); 
 };
